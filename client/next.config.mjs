95fc70dceedbbf9230c454032354a60a0eb4783d
yash-op7 <yashm3375@gmail.com>
2024-09-15 17:40:39 +0530

    fixed the configuration error

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['s3-inventorymanagement.s3.us-east-2.amazonaws.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
